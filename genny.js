188257f53bf673291e58c03bb9039dcc43ab6024
csware <60480473+Constructionware@users.noreply.github.com>
2021-08-04 07:22:27 -0500

    Add files via upload

---
+++ genny.js
@@ -0,0 +1,18 @@
+var genny = function (fn, ln) {
+	var fnn = fn.charAt(0);
+	var lnn = ln.charAt(0); 
+	var head = "", i = 0;
+	/* Create the header */
+	head = head + fnn.toUpperCase() + lnn.toUpperCase()
+	for (i; i < 5; i++) {
+		head = head + Math.floor((Math.random() * 10) + 1)
+		//data.push(Math.floor((Math.random() * 10) + 1))
+	}
+	
+	console.log(head);
+	
+	return head;
+}
+
+genny('or', 'big');
+
